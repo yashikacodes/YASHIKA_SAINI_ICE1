869b687f5faff0edf3000e50ef3cc1d33bdcc521
yashikacodes <sainiyas@sheridancollege.ca>
2023-05-26 17:05:00 -0400

    commit final

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -6,11 +6,13 @@ package card;
 
 //YASHIKA SAINI
 //STUDENT NUMBER - 991708471 
-/**
+/*
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author srinivsi
+ * @modifier yashikacodes
+ * @date modified - 25 may 2023
  */
 import java.util.*;
 public class CardTrick {
